0c113cb1e3e371ad158464927384113eb5d5c878
Emerson <emersonnogueira54321@gmail.com>
2025-05-19 15:30:51 -0300

    feat: component Scroll to top

--- src/Services/Rotas.jsx
+++ src/Services/Rotas.jsx
@@ -6,10 +6,12 @@ import {
 } from "react-router-dom";
 import SeasonOneTutorial from "../Components/SeasonOneTutorial/SeasonOneTutorial";
 import Home from "../Pages/Home/Home";
+import ScrollToTop from "../Components/ScrollToTop/ScrollToTop";
 
 function Rotas() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/season-1" element={<SeasonOneTutorial />} />
